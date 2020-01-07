a9e452a70e5307b07f52ed1192fa89e537c2e619
son loku <jacobrborgen@gmail.com>
2020-01-07 15:05:49 -0600

    added array with all questions answers

--- brains.js
+++ brains.js
@@ -5,7 +5,30 @@ var choiceA = document.getElementById("choice-A");
 var choiceB = document.getElementById("choice-B");
 var choiceC = document.getElementById("choice-C");
 
-
+//Array holding all of the question info
+var questions = [
+    {
+        question : "What does CSS stand for?",
+        optionA : "Computer Science Stuff",
+        optionB : "Cascading Style Sheets",
+        optionC : "Cranky Silly Steve",
+        correct : "B"
+    },
+    {
+        question : "Where do you link your bootstrap CDN?",
+        optionA : "In the Body",
+        optionB : "In CSS",
+        optionC : "In the Head",
+        correct : "C"
+    }
+    {
+        question : "Is Javascript a front-end or back-end language?",
+        optionA : "front-end",
+        optionB : "back-end",
+        optionC: "both",
+        correct : "C"
+    }
+]
 //Begin quiz on start button click
 function initialize() {
     start.style.display = "none";
