c1808e365461a33ee7811f508a551edcd874c9a6
son loku <jacobrborgen@gmail.com>
2020-01-07 14:11:18 -0600

    added basic layout for quiz and grabbed elements with getElementbyId in JS

---
+++ brains.js
@@ -0,0 +1,12 @@
+//Grabbing all elements in my HTML
+var start = document.getElementById("startBtn");
+var quizCtnr = document.getElementById("quiz-container");
+var choiceA = document.getElementById("choice-A");
+var choiceB = document.getElementById("choice-B");
+var choiceC = document.getElementById("choice-C");
+
+
+//Begin quiz on start button click
+function initialize() {
+    start.style.display = "none";
+}
