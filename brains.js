5c7937785c9b906832e9afbae538d5462c848288
son loku <jacobrborgen@gmail.com>
2020-01-09 18:19:21 -0600

    added html and css elements - created hide and show code to display page after start button is pushed. Created timer.

--- brains.js
+++ brains.js
@@ -1,6 +1,19 @@
+//Element Grabbin'
+var timer = document.getElementById("timer");
+var question = document.getElementById("question");
+var buttonA = document.getElementById("btnA");
+var buttonB = document.getElementById("btnB");
+var buttonC = document.getElementById("btnC");
+
 //build a start button on intial page
     //needs to start timer countdown when clicked
     //needs to display 1st question and answer options when clicked
+
+    $("#startBtn").click(function StartQuiz() {
+        $("#start").hide(1000);
+        $("#qaDisplay").show(1000);
+    });
+
 
 //build timer
     //needs to be displayed at 0 on start page
@@ -8,14 +21,64 @@
     //needs to lose 15sec when wrong answer pushed
     //when time hits 0 quiz ends
 
+var counter = 0;
+var countDown = 75;
+
+    $("#startBtn").click(function timeGo() {
+    counter++;
+
+    $("#timer").html(countDown - counter);
+
+    setInterval(timeGo, 1000);
+    });
+
 //need to display questions
     //have empty divs and display through JS?
+    //in an array?
     //show-hide with HTML/CSS?
     //need to store info in an array and when answer is clicked the next question displays
+
+var questions = [
+    {
+        Question : "Here is question 1",
+        AnswerOne : "Option 1",
+        AnswerTwo : "Option 2",
+        AnswerThree : "Option 3"
+    },
+    {
+        Question : "Here is question 1",
+        AnswerOne : "Option 1",
+        AnswerTwo : "Option 2",
+        AnswerThree : "Option 3"
+    },
+    {
+        Question : "Here is question 1",
+        AnswerOne : "Option 1",
+        AnswerTwo : "Option 2",
+        AnswerThree : "Option 3"
+    },
+    {
+        Question : "Here is question 1",
+        AnswerOne : "Option 1",
+        AnswerTwo : "Option 2",
+        AnswerThree : "Option 3"
+    },
+    {
+        Question : "Here is question 1",
+        AnswerOne : "Option 1",
+        AnswerTwo : "Option 2",
+        AnswerThree : "Option 3"
+    }
+];
+
+$("#startBtn").click(function FirstAppend() {
+    $("#question").append();
+});
 
 //display answer options
     //three answers per question
     //same questions on how to display as questions?
+    //in same array as questions?
     //when clicked need to display next question/answers
     //if wrong answer is clicked decrease time by 15sec
 
